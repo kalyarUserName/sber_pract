8e2f79b2df0a1e3f3f646c599c4965bccb946f75
kalyarUserName <ron230298@email.com>
2022-03-22 04:02:40 +0300

    update authorization

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import s from '../styles/Login.module.css'
 import {AuthContext, UserContext} from "../context";
+import {users} from "../users";
 
 const Login = () => {
-  //  const [isAuth, setIsAuth] = useContext(AuthContext);
-     const user = useContext(UserContext);
-    const Auth = useContext(AuthContext);
+    const UserCont = useContext(UserContext);
+    const AuthCont = useContext(AuthContext);
+    const [error, setError] = useState(null);
+    useEffect(()=> {
+        setError(null);
+    },[])
     const login = event => {
         event.preventDefault();
-        console.log(Auth.isAuth, user);
-        Auth.setIsAuth(true);
-        localStorage.setItem('auth','true');
+        setError(null);
+        const {username, password} = event.target.elements;
+        let user = users.find(item => (item.username === username.value) && (item.password === password.value));
+        if (user?.username) {
+            AuthCont.setIsAuth(true);
+            UserCont.setUser(user.username);
+            localStorage.setItem('auth', 'true');
+        } else {
+            setError(true)
+        }
+        console.log(error);
     }
     return (
         <div className={s.loginWrapper}>
-
-            {/*<form onSubmit={login}>*/}
-            {/*    <label>*/}
-            {/*        Name:*/}
-            {/*        <input type="text" placeholder="username"/>*/}
-            {/*    </label>*/}
-            {/*    <label>*/}
-            {/*        Password:*/}
-            {/*        <input placeholder="password"*/}
-            {/*               type="password"/>*/}
-            {/*    </label>*/}
-            {/*    <input type="submit" value="Login"/>*/}
-
-
-            {/*</form>*/}
             <form onSubmit={login}>
-                <input type="text" placeholder="username" onSubmit={event => user.setUser(event.target.value)}/>
-                <input type="password" placeholder="password"/>
-                <button >Login</button>
+                <input type="text" id="username" placeholder="username"
+                       onSubmit={event => UserCont.setUser(event.target.value)}/>
+                <input type="password" id="password" placeholder="password"/>
+                <button>Login</button>
 
             </form>
-            {/*<button onClick={() => localStorage.setItem('isAuthorized', 'false')}> Logout</button>*/}
+            {error!==null ? <h3 className={s.incorrect}>Имя пользователя или пароль введены не верно!</h3> : (<div/>)}
         </div>
     );
 };
---
+++ src/users.js
@@ -0,0 +1,4 @@
+export const users = [
+    {id: 1, username: 'Admin', password: '12345'},
+    {id: 2, username: 'user', password: 'user'}
+];
